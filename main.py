92143200061bd2d62abd616e69c47c1787df696f
Siby C.R <sibycr18@gmail.com>
2023-11-06 00:13:40 +0530

    keeping track

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import streamlit as st
 import text_extractor
+import os
+import shutil
 
-filename = input("Enter filename: ")
-filepath = f"./pdfs/{filename}.pdf"
-print(f"Extracted Text: {text_extractor.extract_text_from_pdf(filepath)[:100]}")
+st.title("summarizED - PDF Summarizer")
 
+target_directory = './pdfs'
+shutil.copy(source_file, target_directory)
+
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # Get the file path
+    # file_path = os.path.join("pdfs", filename)  # Save the file in the 'uploads' folder
+
+    filename = uploaded_file.name
+    file_extension = os.path.splitext(file_name)[1]
+
+
+# filename = input("Enter filename: ")
+# filepath = f"./pdfs/{filename}.pdf"
+extracted_text = text_extractor.extract_text_from_pdf(file_path)
+print(f"Extracted Text: {extracted_text}")
+
+st.write(extracted_text)
