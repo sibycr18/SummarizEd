e9982a4b29d487991f43bd28fe3dc8019e7bfd5b
Siby C.R <sibycr18@gmail.com>
2023-11-06 22:20:03 +0530

    set up pdf upload and read

--- main.py
+++ main.py
@@ -1,26 +1,68 @@
 import streamlit as st
-import text_extractor
-import os
-import shutil
+import chromadb
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from PyPDF2 import PdfReader
 
-st.title("summarizED - PDF Summarizer")
+## Intitialization
+# Intialize ChromaDB
+@st.cache_resource
+def init_db():
+    db_client = chromadb.PersistentClient(path="./db")
+    return db_client
 
-target_directory = './pdfs'
-shutil.copy(source_file, target_directory)
+# Initialize Embeddings
+@st.cache_resource
+def init_embedding():
+    embeddings = HuggingFaceEmbeddings(model_name="infgrad/stella-base-en-v2")
+    return embeddings
+
+# Session states
+db_client = st.session_state.db_client = init_db()
+embeddings = st.session_state.embeddings = init_embedding()
+
+# Already uploaded files
+collections = st.session_state.db_client.list_collections()
+
+## App Title
+st.title("Summariz:orange[Ed] :gray[- PDF Summarizer]", )
+# st.divider()
+st.subheader("", divider="gray")    # maybe not be a proper way but i like this
 
 
-uploaded_file = st.file_uploader("Choose a file")
+# Display file uploader
+uploaded_file = st.file_uploader("Upload a new PDF file", type=["pdf"])
+# st.button("Process PDF", type="primary", disabled=True)
+
 if uploaded_file is not None:
-    # Get the file path
-    # file_path = os.path.join("pdfs", filename)  # Save the file in the 'uploads' folder
+    file_name = uploaded_file.name
+    # Read and display the content of the PDF file
+    pdf_reader = PdfReader(uploaded_file)
+    pdf_text = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        pdf_text += page.extract_text()
 
-    filename = uploaded_file.name
-    file_extension = os.path.splitext(file_name)[1]
+    st.button("Process PDF", type="primary")
+
+    # st.caption(f"File Name: {file_name}")
+
+st.subheader("OR")
+
+pdf_list = tuple(collection.name for collection in collections)
+placeholder = "Select the PDF file..." if len(pdf_list) > 0 else "No PDFs uploaded"
+selected_file = st.selectbox(
+   "Already uploaded?",
+   pdf_list,
+   index=None,
+   placeholder = placeholder,
+)
+
+# Print file content
+if uploaded_file is not None:
+    st.divider()
+    st.header("PDF Content:")
+    st.write(pdf_text)
 
 
-# filename = input("Enter filename: ")
-# filepath = f"./pdfs/{filename}.pdf"
-extracted_text = text_extractor.extract_text_from_pdf(file_path)
-print(f"Extracted Text: {extracted_text}")
-
-st.write(extracted_text)
+##
